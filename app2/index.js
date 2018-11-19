5cdb86a4a9722ddb364ac3b7da2db2f8cfcbac52
GudoIlya <goodilya777@gmail.com>
2018-11-19 23:53:06 +0300

    Start Redux tutorial

---
+++ app2/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux"
+import todoApp from "./reducers"
+
+import {addTodo, setVisibilityFilter, toggleTodo, VisibilityFilters} from "./actions";
+
+const store = createStore(todoApp);
+
+console.log(store.getState);
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+});
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+// Stop listening to state updates
+unsubscribe();
