6383176ef6fb6f42994329c456fc868e5289a4c7
igudov <gudovi@stroy-online.ru>
2018-11-27 12:43:27 +0300

    next step

--- app3/driver.js
+++ app3/driver.js
@@ -2,22 +2,22 @@ window._ = require('underscore');
 var Backbone = require('backbone');
 var Marionette  = require('backbone.marionette');
 
-var TodoList = Marionette.LayoutView.extend({
+var ToDo = Marionette.LayoutView.extend({
+    tagName : 'li',
+    template : require('./templates/todoitem.html')
+});
+
+var TodoList = Marionette.CollectionView.extend({
     el : '#main',
-    template : require('./templates/layout.html')
+    tagName : 'ul',
+    childView : ToDo
 });
 
 var todo = new TodoList({
-    model : new Backbone.Model({
-        items : [
-            {
-                assignee: "Scot", text : "Write a book about Marionette"
-            },
-            {
-                assignee: "Andrew", text : "Do some coding"
-            }
-        ]
-    })
+    collection : new Backbone.Collection([
+        {assignee: 'Scott', text: 'Write a book about Marionette'},
+        {assignee: 'Andrew', text: 'Do some coding'}
+    ])
 });
 
 todo.render();
